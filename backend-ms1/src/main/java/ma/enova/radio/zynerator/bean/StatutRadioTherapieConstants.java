a624f0485ee6dfe08718ff9d3100da6b4815cd78
ismail barhdadi <ismail.baghdadi@my-enova.com>
2023-02-14 15:15:29 +0100

    add StatutRadioTherapie Constants

---
+++ backend-ms1/src/main/java/ma/enova/radio/zynerator/bean/StatutRadioTherapieConstants.java
@@ -0,0 +1,12 @@
+package ma.enova.radio.zynerator.bean;
+
+public class StatutRadioTherapieConstants {
+	
+	public static final String ENATTENTEPRESCRIPTION = "en-attente-prescription";
+	public static final String ENATTENTESIMULATION = "en-attente-simulation";
+	public static final String ENATTENTEVALIDATION = "en-attente-validation";
+	public static final String ENCOURSTRAITEMENT = "en-cours-traitement";
+	public static final String FINTRAITEMENT = "fin-traitement";
+
+
+}
